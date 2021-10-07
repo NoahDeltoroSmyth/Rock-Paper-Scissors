d5546a5a537390ce1394e67f48ff5def375b50b6
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2021-10-06 17:41:46 -0700

    created qunit tests. I know they are supposed to go before actually writing the code, but today was a struggle

--- test/example.test.js
+++ test/example.test.js
@@ -7,7 +7,7 @@ const test = QUnit.test;
 test('user: rock, computer: rock', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = ;
+    const expected = 'draw';
     
     //Act 
     // Call the function you're testing and set the result to a const
@@ -15,5 +15,17 @@ test('user: rock, computer: rock', (expect) => {
 
     //Expect
     // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('user: paper, computer: scissors', (expect) => {
+    const expected = 'lose';
+    const actual = didUserWin('paper', 'scissors');
+    expect.equal(actual, expected);
+});
+
+test('user: scissors, computer: paper', (expect) => {
+    const expected = 'win';
+    const actual = didUserWin('scissors', 'paper');
     expect.equal(actual, expected);
 });
