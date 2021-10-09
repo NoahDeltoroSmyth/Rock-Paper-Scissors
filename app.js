53d6776a11bcac52a4fea199328ec09baa624e48
Hayden Indiana Holdsworth <IndianaHoldsworth@gmail.com>
2021-10-08 17:23:18 -0700

    Adds working reset button and reset counter

--- app.js
+++ app.js
@@ -8,23 +8,27 @@ const Draw = document.getElementById('draws');
 const Error = document.getElementById('error');
 const Result = document.getElementById('result');
 const resetButton = document.getElementById('reset-button');
+const resetCounter = document.getElementById('reset-counter');
 
 // initialize global state
 let wins = 0;
 let losses = 0;
 let draws = 0;
-        
+let resets = 0;
 // set event listeners 
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
 Button.addEventListener('click', ()=>{
-  //console.log(getRandomThrow());
+    //console.log(getRandomThrow());
     const selected = document.querySelector('input[type=radio]:checked'); 
     if (!selected){
         return Error.classList.remove('hidden');
     }
     Error.classList.add('hidden');
+    
+    resetButton.disabled = false;
+    Result.hidden = false;
 
     const userChoice = selected.value;
     const randomThrow = getRandomThrow();
@@ -41,6 +45,7 @@ Button.addEventListener('click', ()=>{
         Result.textContent = 'You drew! The computer threw the same!';
     }
     
+
     Win.textContent = wins;
     Loss.textContent = losses;
     Draw.textContent = draws;
@@ -48,12 +53,23 @@ Button.addEventListener('click', ()=>{
 });
 
 resetButton.addEventListener('click', () => {
+    resets++;
+    resetCounter.textContent = resets;
+
+    wins = 0;
+    losses = 0;
+    draws = 0;
+
     const selected = document.querySelector('input[type=radio]:checked'); 
 
-    selected.checked  = false;
+    selected.checked = false;
 
-    Win.textContent = 0;
-    Loss.textContent = 0;
-    Draw.textContent = 0;
+    Win.textContent = wins;
+    Loss.textContent = losses;
+    Draw.textContent = draws;
+
+    Result.hidden = true;
+
+    resetButton.disabled = true;
 
 });
